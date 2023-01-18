51c7548e027e079137d201d20d127b40f5883374
deji12 <adesolaayodeji53@gmail.com>
2023-01-18 14:15:04 +0300

    edited models and added key

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Room(models.Model):
     room_name = models.CharField(max_length=255)
+    key = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.room_name
